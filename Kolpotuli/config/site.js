b790e84ffa3fd5b870217c4cf82e3f87cb24ecac
Rudra Narayan Mandal <p.rudra2805@gmail.com>
2026-09-14 03:16:57 +0530

    Add central Kolpotuli configuration

---
+++ Kolpotuli/config/site.js
@@ -0,0 +1,16 @@
+export const SITE = {
+  name: 'Kolpotuli',
+  bengali: 'কল্পতুলি',
+  instagram: 'https://www.instagram.com/kolpotuli/',
+  email: '',
+  brand: { paper: '#E9D083', ink: '#1E2D56', gold: '#FFBF00', grey: '#B8C0CC', outline: '#111111' },
+  footer: {
+    bn: 'ছাড়িয়া মোর পিছু,\nযাও হেথা হতে সনাতন',
+    en: 'Alone, abandoned, my heart did swoon,\nIn a city where even love had gone too soon.'
+  }
+};
+
+export const NAV = [
+  ['home', 'Home'], ['stories', 'Stories'], ['art', 'My Art'],
+  ['blogs', 'Blogs'], ['about', 'About Me'], ['contact', 'Contact']
+];
